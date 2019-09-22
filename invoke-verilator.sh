56b01ecb1b9b4a2facb1a4480d66992c945e2e2b
Keun Soo Cho <keunsoo.cho@gmail.com>
2019-09-22 09:54:31 +0900

    autocomplete comment out

--- invoke-verilator.sh
+++ invoke-verilator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -f .lint ];then
-    exec verilator $@ -f .lint -Wall -Wno-PINCONNECTEMPTY -Wno-DECLFILENAME -Wno-DEFPARAM
+    exec verilator -f .lint $@ -Wall -Wno-PINCONNECTEMPTY -Wno-DECLFILENAME -Wno-DEFPARAM
 else
     exec verilator $@ -Wall -Wno-PINCONNECTEMPTY -Wno-DECLFILENAME -Wno-DEFPARAM
 fi
